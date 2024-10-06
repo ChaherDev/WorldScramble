44c97c811aeaa38540fae44507233c564771f973
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-06 21:19:45 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Chaher Machhour on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
